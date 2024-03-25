3ba98ae17e551c54a1e7e1d858e94decdcf0b15d
ZaenulHakk <164845662+ZaenulHakk@users.noreply.github.com>
2024-03-25 20:29:48 +0800

    Add files via upload

---
+++ tugas jenong.py
@@ -0,0 +1,24 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nama=("zaenul haki")
+>>> umur=int(18)
+>>> nim=("si20230036")
+>>> semester=("dua")
+>>> alamat=("kadindi")
+>>> lulusan=("ma darul kamilin")
+>>> print("=="*10)
+====================
+>>> print("selamat datang",nama)
+selamat datang zaenul haki
+>>> print("umur anda",umur)
+umur anda 18
+>>> print("nim anda",nim)
+nim anda si20230036
+>>> print("anda semester",semester)
+anda semester dua
+>>> print("alamat anda",alamat)
+alamat anda kadindi
+>>> print("anda lulusan",lulusan)
+anda lulusan ma darul kamilin
+>>> print("=="*10)
+====================
